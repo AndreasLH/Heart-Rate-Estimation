41bacb7e4d0ec408e787c18f666bb170a5fa58b1
Andreas Lau Hansen <s194235@student.dtu.dk>
2022-03-21 17:01:51 +0100

    gaussian noise works

--- convert_videos.sh
+++ convert_videos.sh
@@ -1,3 +1,8 @@
 # converts all avi videoes in all subfolders to mp4
 cd cpi
+
+# unzip -o *.zip
+
 find . -type f -name "*.avi" -exec bash -c 'FILE="$1"; ffmpeg -i "${FILE}"  "${FILE%.avi}.mp4";' _ '{}' \;
+
+find . -type f -name "*.avi" -exec bash -c 'FILE="$1"; rm "${FILE}";' _ '{}' \;
